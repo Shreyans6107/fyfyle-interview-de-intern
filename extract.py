f96bbea1b7aad3151be330dfb017b945a2b0f2d1
shreyans <shreyans.gupta6107@gmail.com>
2021-09-07 13:25:58 +0530

    final

--- extract.py
+++ extract.py
@@ -1,5 +1,8 @@
 # Your imports go here
 import logging
+import re
+import os
+import json
 
 logger = logging.getLogger(__name__)
 
@@ -15,7 +18,15 @@ logger = logging.getLogger(__name__)
 '''
 def extract_amount(dirpath: str) -> float:
 
-    logger.info('extract_amount called for dir %s', dirpath)
-    # your logic goes here
+	logger.info('extract_amount called for dir %s', dirpath)
 
-    return 0.0
+	for x in os.listdir(dirpath):
+		if 'ocr' in x:
+			with open(os.path.join(dirpath, x)) as f:
+				data = json.load(f)
+	prices = []
+	for x in data['Blocks']:
+		if x.get('Text'):
+			if re.search(r'[0-9]*\.[0-9][0-9]', x['Text']):
+				prices.append(float(re.sub("[^0-9.]", '', x['Text'])))
+	return max(prices)
